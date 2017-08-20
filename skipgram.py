eeca29a888cf16aad12db2c5f849072500dbc67f
dev <dev@ubuntu-server01.devdomain>
2017-08-20 22:47:42 +1000

    ported skipgram.py

--- skipgram.py
+++ skipgram.py
@@ -3,20 +3,21 @@ import numpy as np
 import tensorflow as tf
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 # TRAINING PARAMETERS
 batch_size = 32                                             # Number of training examples per batch
 embedding_size = 128                                        # Dimension of embedding vectors
 skip_window = 5                                             # Window size for context to the left and right of target
 num_skips = 4                                               # How many times to reuse target to generate a label for context.
-batches_per_epoch = data.data_size*num_skips/batch_size     # Number of batches per epoch of training
+batches_per_epoch = int(data.data_size*num_skips/batch_size) # Number of batches per epoch of training
 training_epochs = 5                                         # Number of epochs to utilize for training
 neg_size = 64                                               # Number of negative samples to use for NCE
 display_step = 2000                                         # Frequency with which to print statistics
 val_step = 10000                                            # Frequency with which to perform validation
 learning_rate = 0.1                                         # Learning rate for SGD
 
-print "Epochs: %d, Batches per epoch: %d, Examples per batch: %d" % (training_epochs, batches_per_epoch, batch_size)
+print("training parameters :: Epochs: {:04d}:, Batches per epoch: {:d}, Examples per batch: {:d}".format(training_epochs, batches_per_epoch, batch_size))
 
 # NEAREST NEIGHBORS VALIDATION PARAMETERS
 val_size = 20
@@ -39,12 +40,14 @@ def noise_contrastive_loss(embedding_lookup, weight_shape, bias_shape, y):
         nce_W = tf.get_variable("W", initializer=nce_weight_init)
         nce_b = tf.get_variable("b", initializer=nce_bias_init)
 
-        total_loss = tf.nn.nce_loss(nce_W, nce_b, embedding_lookup, y, neg_size, data.vocabulary_size)
+        # TF 1.2.x changed order of arguments :( 
+        # total_loss = tf.nn.nce_loss(nce_W, nce_b, embedding_lookup, y, neg_size, data.vocabulary_size)
+        total_loss = tf.nn.nce_loss(weights=nce_W, biases=nce_b, labels=y, inputs=embedding_lookup, num_sampled=neg_size, num_classes=data.vocabulary_size)
         return tf.reduce_mean(total_loss)
 
 def training(cost, global_step):
     with tf.variable_scope("training"):
-        summary_op = tf.scalar_summary("cost", cost)
+        summary_op = tf.summary.scalar("cost", cost)
         optimizer = tf.train.GradientDescentOptimizer(learning_rate)
         train_op = optimizer.minimize(cost, global_step=global_step)
         return train_op, summary_op
@@ -57,13 +60,16 @@ def validation(embedding_matrix, x_val):
     return normalized, cosine_similarity
 
 if __name__ == '__main__':
-
+    start_time = datetime.now()
+    print("{}:: training model - BEGIN".format(start_time))
     with tf.Graph().as_default():
 
         with tf.variable_scope("skipgram_model"):
 
-            x = tf.placeholder(tf.int32, shape=[batch_size])
-            y = tf.placeholder(tf.int32, [batch_size, 1])
+            x = tf.placeholder(tf.int32, shape=[batch_size]) 
+            # original - y must be of type tf.float32
+            # y = tf.placeholder(tf.int32, [batch_size, 1])
+            y = tf.placeholder(tf.float32, [batch_size, 1])
             val = tf.constant(val_examples, dtype=tf.int32)
             global_step = tf.Variable(0, name='global_step', trainable=False)
 
@@ -75,19 +81,22 @@ if __name__ == '__main__':
 
             val_op = validation(e_matrix, val)
 
-            sess = tf.Session()
+            runtimeConfig = tf.ConfigProto()
+            runtimeConfig.gpu_options.allow_growth = True
 
-            train_writer = tf.train.SummaryWriter("skipgram_logs/", graph=sess.graph)
+            sess = tf.Session(config=runtimeConfig)
 
-            init_op = tf.initialize_all_variables()
+            train_writer = tf.summary.FileWriter("tf_events/skipgram_logs/", sess.graph)
+
+            init_op = tf.global_variables_initializer()
 
             sess.run(init_op)
 
             step = 0
             avg_cost = 0
 
-            for epoch in xrange(training_epochs):
-                for minibatch in xrange(batches_per_epoch):
+            for epoch in range(training_epochs):
+                for minibatch in range(batches_per_epoch):
 
                     step +=1
 
@@ -100,24 +109,31 @@ if __name__ == '__main__':
                     avg_cost += new_cost/display_step
 
                     if step % display_step == 0:
-                        print "Elapsed:", str(step), "batches. Cost =", "{:.9f}".format(avg_cost)
+                        current_time = datetime.now()
+                        print("{} :: Elapsed: {}, batches. Cost = {:.9f}".format(current_time, str(step), avg_cost))
                         avg_cost = 0
 
                     if step % val_step == 0:
                         _, similarity = sess.run(val_op)
-                        for i in xrange(val_size):
+                        for i in range(val_size):
                             val_word = data.reverse_dictionary[val_examples[i]]
                             neighbors = (-similarity[i, :]).argsort()[1:top_match+1]
                             print_str = "Nearest neighbor of %s:" % val_word
-                            for k in xrange(top_match):
+                            for k in range(top_match):
                                 print_str += " %s," % data.reverse_dictionary[neighbors[k]]
-                            print print_str[:-1]
+                            print(print_str[:-1])
 
             final_embeddings, _ = sess.run(val_op)
 
+    end_time = datetime.now()
+    print("{}:: training model - DONE".format(end_time))
+    elapsed_time = end_time - start_time
+    print("elapsed time (seconds) = {}".format(elapsed_time.total_seconds()))
 
+    # comment out until I have window display or on Jupyter
     tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
     plot_embeddings = np.asfarray(final_embeddings[:plot_num,:], dtype='float')
     low_dim_embs = tsne.fit_transform(plot_embeddings)
-    labels = [reverse_dictionary[i] for i in xrange(plot_only)]
+    labels = [reverse_dictionary[i] for i in range(plot_only)]
     data.plot_with_labels(low_dim_embs, labels)
+
